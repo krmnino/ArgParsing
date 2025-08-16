c2909eea090eb017faa41111cc4ff8bcd4ed5db3
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-08-15 23:52:20 -0400

    created main loop for building testcases and execute them

--- testing/Main.cpp
+++ testing/Main.cpp
@@ -1,21 +1,63 @@
 #include "ArgParsingTesting.hpp"
 
-Randomizer* Randomizer::rnd_ptr = nullptr;
+#include <atomic>
+#include <signal.h>
 
-int run_testcase(Randomizer* rnd){
-    //ArgParsing* ap_ptr = new ArgParsing();
-    //std::vector<APTableEntry> arg_table;
-    TestcaseData tdata;
-    std::string scenario_argv;
-    size_t n_scenarios;
-    uint32_t scenario_type_pool;
-    uint32_t picked_scenario_type;
-    uint32_t attempt_counter;
+Randomizer* Randomizer::rnd_ptr = nullptr;
+static std::atomic_bool terminate = false;
+
+void terminating(int s){
+    terminate = true;
+}
+
+int main(int argc, char* argv[]){
+    ArgParsing* pgm_ap;
+    ArgParsing* ap_test;
+    Randomizer* rnd;
+    TestcaseData* testcase;
+    uint64_t counter;
+    uint64_t n_tests;
+    uint64_t n_scenarios;
+    uint32_t init_seed;
     uint32_t user_allowed_scenario_types;
-    uint32_t shifter;
-    ScenarioData sdata;
-    int ret;
+    bool infinite_loop;
+    bool terminate;
+
+    // Program argument table 
+    APTableEntry arg_table[] = {
+        { "s", "seed"       , APDataType::NUMBER  , true  },
+        { "t", "n_tests"    , APDataType::NUMBER  , true  },
+        { "c", "n_scenarios", APDataType::NUMBER  , true  },
+        { "r", "trace"      , APDataType::FLAG    , false },
+    };
     
+    // Start the argument parser
+    pgm_ap = new ArgParsing();
+    pgm_ap->set_arg_table(arg_table, sizeof(arg_table) / sizeof(APTableEntry));
+    pgm_ap->set_input_args(argc, argv);
+    if(pgm_ap->parse() != 0){
+        return -1;
+    }
+
+    // Start the Randomizer
+    init_seed = 1;
+    rnd = Randomizer::get_instance(init_seed);
+    if(rnd == nullptr){
+        return -1;
+    }
+
+    // Set up signal handler to stop program
+    terminate = false;
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = terminating;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    counter = 0;
+    n_tests = 1;
+    n_scenarios = 1;
+    infinite_loop = false;
     user_allowed_scenario_types = (uint32_t)ScenarioType::OK                     ;//|
                                   //(uint32_t)ScenarioType::MISSING_FIRST_DASH     |
                                   //(uint32_t)ScenarioType::MISSING_REQUIRED_ARG   |
@@ -27,74 +69,23 @@ int run_testcase(Randomizer* rnd){
                                   //(uint32_t)ScenarioType::VALID_FLAG_GROUP       |
                                   //(uint32_t)ScenarioType::INVALID_FLAG_GROUP     ;
     
-    // Attempt building a argument table and check scenarios that can be tested
-    attempt_counter = 0;
-    while(true){
-        if(attempt_counter > BUILD_MAX_ATTEMPTS){
-            return -1;
+    // Main driver
+    while((counter < n_tests || infinite_loop) && !terminate){
+        testcase = new TestcaseData();
+        // Build a testcase and its multiple scenarios
+        build_testcase(rnd, *testcase, n_scenarios, user_allowed_scenario_types);
+        for(size_t i = 0; i < n_scenarios; i++){
+            ap_test = new ArgParsing();
+            ap_test->set_arg_table(testcase->ini_argtab);
+            ap_test->set_input_args(testcase->s_arr[i].argc, testcase->s_arr[i].argv);
+            ap_test->parse();
+            delete ap_test;
         }
-        ret = build_initial_arg_table(rnd, tdata);
-        // If returned -1, try build another argument table
-        if(ret == -1){
-            continue;
-        }
-        scenario_type_pool = check_allowed_scenarios(tdata.ini_argtab, user_allowed_scenario_types);
-        // If returned scenario pool is zero/empty, try build another argument table
-        if(scenario_type_pool != 0x00000000){
-            break;
-        }
-        attempt_counter++;
+        delete testcase;
+        counter++;
     }
 
-    // Generate scenarios based on argument table and allowed scenario types
-    n_scenarios = 100;
-    tdata.scenario.reserve(n_scenarios);
-    for(size_t i = 0; i < n_scenarios; i++){
-        // Pick a single scenario from the pool
-        attempt_counter = 0;
-        while(true){
-            if(attempt_counter > BUILD_MAX_ATTEMPTS){
-                return -1;
-            }
-            shifter = rnd->gen_integral_range<uint32_t>(0, MAX_TYPES);
-            picked_scenario_type = (1 << shifter);
-            if((scenario_type_pool & picked_scenario_type) != 0){
-                break;
-            }
-            attempt_counter++;
-        }
-        sdata.type = (ScenarioType)picked_scenario_type;
-
-        // Copy initial arg table to scenario expected table data 
-        sdata.exp_argtab = tdata.ini_argtab;
-
-        // Build the argv for scenario
-        build_scenario(rnd, sdata);
-        std::cout << arg_table_to_string(sdata.exp_argtab) << std::endl;
-        
-        //std::cout << scenario_argv << std::endl;
-    }
-
-    
-    //delete ap_ptr;
-    return 0;
-}
-
-int main(int argc, char* argv[]){
-    uint32_t init_seed;
-    uint32_t n_tests;
-    
-    init_seed = 1;
-    Randomizer* rnd = Randomizer::get_instance(init_seed);
-    if(rnd == nullptr){
-        return -1;
-    }
-
-    n_tests = 1;
-    for(size_t i = 0; i < n_tests; i++){
-        run_testcase(rnd);
-    }
-
+    delete pgm_ap;
     Randomizer::end_instance();
     return 0;
 }
