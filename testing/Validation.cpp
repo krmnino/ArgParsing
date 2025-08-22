fe035eb238a88495519a7aabd08c0f3459199880
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-08-21 23:56:00 -0400

    log test results when validating scenario OK

--- testing/Validation.cpp
+++ testing/Validation.cpp
@@ -14,27 +14,32 @@ void validate(ErrorReporter* er, uint32_t seed, size_t tc_counter, TestcaseData&
         er->log_it(buffer);
         er->log_it(">>> END OF INITIAL ARGUMENT TABLE <<<");
         switch (tc.s_arr[i].type){
-        case ScenarioType::OK:
+            case ScenarioType::OK:
             validate_OK_scenario(er, tc.s_arr[i]);
             break;
         default:
-            break;
+        break;
         }
         er->end_test();
     }
 }
 
 void validate_OK_scenario(ErrorReporter* er, ScenarioData& sc){
+    std::string buffer;
     // Result vs. Expected error mesage
+    er->log_it("Result   : res_error_message = \"" + sc.res_error_message + "\"");
+    er->log_it("Expected : exp_error_message = \"" + sc.exp_error_message + "\"");
     if(sc.res_error_message != sc.exp_error_message){
         er->mark_error();
-        std::cout << "TODO: ERROR 1" << std::endl;
+        er->log_it("!!! ERROR: error_message MISMATCH");
         return;
     }
     // Result vs. Expected argument table size
+    er->log_it("Result   : size of result argtab = " + std::to_string(sc.res_argtab.size()));
+    er->log_it("Expected : size of result argtab = " + std::to_string(sc.exp_argtab.size()));
     if(sc.res_argtab.size() != sc.exp_argtab.size()){
         er->mark_error();
-        std::cout << "TODO: ERROR 2" << std::endl;
+        er->log_it("!!! ERROR: size of argtab MISMATCH");
         return;
     }
     // Result vs. Expected argument tables
@@ -42,34 +47,60 @@ void validate_OK_scenario(ErrorReporter* er, ScenarioData& sc){
         // abbr_form field should not be altered
         if(sc.res_argtab[i].abbr_form != sc.exp_argtab[i].abbr_form){
             er->mark_error();
-            er->log_it("ERROR: abbr_form FIELD MISMATCH");
+            er->log_it("!!! ERROR: abbr_form FIELD MISMATCH");
+            er->log_it("Index    : " + std::to_string(i));
+            er->log_it("Result   : abbr_form = " + sc.res_argtab[i].abbr_form);
+            er->log_it("Expected : abbr_form = " + sc.exp_argtab[i].abbr_form);
         }
         // full_form field should not be altered
         if(sc.res_argtab[i].full_form != sc.exp_argtab[i].full_form){
             er->mark_error();
-            er->log_it("ERROR: full_form FIELD MISMATCH");
+            er->log_it("!!! ERROR: full_form FIELD MISMATCH");
+            er->log_it("Index    : " + std::to_string(i));
+            er->log_it("Result   : full_form = " + sc.res_argtab[i].full_form);
+            er->log_it("Expected : full_form = " + sc.exp_argtab[i].full_form);
         }
         // data_type field should not be altered
         if(sc.res_argtab[i].data_type != sc.exp_argtab[i].data_type){
             er->mark_error();
-            er->log_it("ERROR: data_type FIELD MISMATCH");
+            er->log_it("!!! ERROR: data_type FIELD MISMATCH");
+            er->log_it("Index    : " + std::to_string(i));
+            er->log_it("Result   : data_type = " + APDataType_to_string(sc.res_argtab[i].data_type));
+            er->log_it("Expected : data_type = " + APDataType_to_string(sc.exp_argtab[i].data_type));
         }
         // required field should not be altered
         if(sc.res_argtab[i].required != sc.exp_argtab[i].required){
             er->mark_error();
-            er->log_it("ERROR: required FIELD MISMATCH");
+            er->log_it("!!! ERROR: required FIELD MISMATCH");
+            er->log_it("Index    : " + std::to_string(i));
+            er->log_it("Result   : required = " + bool_to_string(sc.res_argtab[i].required));
+            er->log_it("Expected : required = " + bool_to_string(sc.exp_argtab[i].required));
         }
         // initialized should match the expected
         if(sc.res_argtab[i].initialized != sc.exp_argtab[i].initialized){
             er->mark_error();
-            er->log_it("ERROR: initialized FIELD MISMATCH");
+            er->log_it("!!! ERROR: initialized FIELD MISMATCH");
+            er->log_it("Index    : " + std::to_string(i));
+            er->log_it("Result   : initialized = " + bool_to_string(sc.res_argtab[i].initialized));
+            er->log_it("Expected : initialized = " + bool_to_string(sc.exp_argtab[i].initialized));
         }
         // value should match the expected
         if(sc.res_argtab[i].value != sc.exp_argtab[i].value){
             er->mark_error();
-            er->log_it("ERROR: value FIELD MISMATCH");
+            er->log_it("!!! ERROR: value FIELD MISMATCH");
+            er->log_it("Index    : " + std::to_string(i));
+            er->log_it("Result   : value = " + sc.res_argtab[i].value);
+            er->log_it("Expected : value = " + sc.exp_argtab[i].value);
         }
     }
+    er->log_it(">>> START OF RESULT ARGUMENT TABLE <<<");
+    buffer = arg_table_to_string(sc.res_argtab);
+    er->log_it(buffer);
+    er->log_it(">>> END OF RESULT ARGUMENT TABLE <<<");
+    er->log_it(">>> START OF EXPECTED ARGUMENT TABLE <<<");
+    buffer = arg_table_to_string(sc.exp_argtab);
+    er->log_it(buffer);
+    er->log_it(">>> END OF EXPECTED ARGUMENT TABLE <<<");
 }
 
 void collect_ap_data(ScenarioData& scenario, ArgParsing* ap){
