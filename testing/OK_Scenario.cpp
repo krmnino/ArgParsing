e38af5140b9a2165bd3d6aebc7fcfbda2b77c375
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-09-30 20:19:07 -0400

    updated OK scenario to support appropiate argument data types in builder and validation methods

--- testing/OK_Scenario.cpp
+++ testing/OK_Scenario.cpp
@@ -6,8 +6,9 @@ void build_OK_scenario(Randomizer* rnd, ScenarioData& sc){
     std::vector<std::string> argv;
     std::string arg_id;
     std::string no_dashes_arg_id;
-    std::string value;
+    std::string value_for_argv;
     std::string flag_value;
+    union data value{};
     size_t rand_idx;
     size_t n_initialized;
     uint32_t result_u32;
@@ -88,19 +89,21 @@ void build_OK_scenario(Randomizer* rnd, ScenarioData& sc){
         // Generate data for arguments that need it
         use_flag_value = false;
         switch (sc.exp_argtab[arg_table_idx].data_type){
-        case APDataType::NUMBER:
+        case APDataType::UNSIGNED_INT:
+            value.intdata.number_u64 = rnd->gen_integral<uint64_t>();
             // Pick between hex or decimal
             result_bool = rnd->gen_bool();
             if(result_bool){
-                value = integer_to_hex_string(rnd->gen_integral<int64_t>());
+                value_for_argv = integer_to_hex_string(value.intdata.number_u64);
             }
             else{
-                value = std::to_string(rnd->gen_integral<int64_t>());
+                value_for_argv = std::to_string(value.intdata.number_u64);
             }
             break;    
         case APDataType::TEXT:
             result_u32 = rnd->gen_integral_range<uint32_t>(1, MAX_TEXT_ARG_LEN);
-            value = rnd->gen_string(result_u32, nullptr);
+            value.text = new std::string(rnd->gen_string(result_u32, nullptr));
+            value_for_argv = *value.text;
             break;    
         case APDataType::FLAG:
             use_flag_value = rnd->gen_bool();
@@ -108,10 +111,10 @@ void build_OK_scenario(Randomizer* rnd, ScenarioData& sc){
             if(use_flag_value){
                 result_u32 = rnd->gen_integral_range<uint32_t>(0, (sizeof(valid_flag_values) / sizeof(valid_flag_values[0])) -1);
                 flag_value = valid_flag_values[result_u32];
-                value = valid_flag_values_dict.at(flag_value);
+                value.flag = valid_flag_values_dict.at(flag_value);
             }
             else{
-                value = "1";
+                value.flag = true;
             }
             break;    
         default:
@@ -119,12 +122,27 @@ void build_OK_scenario(Randomizer* rnd, ScenarioData& sc){
         }
 
         // Set argument value
-        sc.exp_argtab[arg_table_idx].value = value;
+        switch(sc.exp_argtab[arg_table_idx].data_type){
+        case APDataType::UNSIGNED_INT:
+            sc.exp_argtab[arg_table_idx].data.intdata.number_u64 = value.intdata.number_u64;
+            break;
+        case APDataType::SIGNED_INT:
+            sc.exp_argtab[arg_table_idx].data.intdata.number_i64 = value.intdata.number_i64;
+            break;
+        case APDataType::TEXT:
+            sc.exp_argtab[arg_table_idx].data.text = new std::string(*value.text);
+            break;
+        case APDataType::FLAG:
+            sc.exp_argtab[arg_table_idx].data.flag = value.flag;
+            break;
+        default:
+            break;
+        }
 
         // Update the argv vector with argument we just created
         argv.push_back(arg_id);
         if(sc.exp_argtab[arg_table_idx].data_type != APDataType::FLAG){
-            argv.push_back(value);
+            argv.push_back(value_for_argv);
         }
         else if(sc.exp_argtab[arg_table_idx].data_type == APDataType::FLAG && use_flag_value){
             argv.push_back(flag_value);
@@ -132,7 +150,7 @@ void build_OK_scenario(Randomizer* rnd, ScenarioData& sc){
 
         // Update argc appropiately
         switch (sc.exp_argtab[arg_table_idx].data_type){
-        case APDataType::NUMBER:
+        case APDataType::UNSIGNED_INT:
         case APDataType::TEXT:
             sc.argc += 2;
             break;
@@ -146,6 +164,11 @@ void build_OK_scenario(Randomizer* rnd, ScenarioData& sc){
             break;
         default:
             break;
+        }
+
+        // Deallocate text value if used
+        if(sc.exp_argtab[arg_table_idx].data_type == APDataType::TEXT){
+            delete value.text;
         }
     }
 
@@ -212,12 +235,47 @@ void validate_OK_scenario(ErrorReporter* er, ScenarioData& sc){
             er->log_it("Expected : initialized = " + bool_to_string(sc.exp_argtab[i].initialized));
         }
         // value should match the expected
-        if(sc.res_argtab[i].value != sc.exp_argtab[i].value){
-            er->mark_error();
-            er->log_it("!!! ERROR: value FIELD MISMATCH");
-            er->log_it("Index    : " + std::to_string(i));
-            er->log_it("Result   : value = " + sc.res_argtab[i].value);
-            er->log_it("Expected : value = " + sc.exp_argtab[i].value);
+        if(sc.exp_argtab[i].initialized){
+            switch(sc.exp_argtab[i].data_type){
+            case APDataType::UNSIGNED_INT:
+                if(sc.res_argtab[i].data.intdata.number_u64 != sc.exp_argtab[i].data.intdata.number_u64){
+                    er->mark_error();
+                    er->log_it("!!! ERROR: value FIELD MISMATCH");
+                    er->log_it("Index    : " + std::to_string(i));
+                    er->log_it("Result   : value = " + sc.res_argtab[i].data.intdata.number_u64);
+                    er->log_it("Expected : value = " + sc.exp_argtab[i].data.intdata.number_u64);
+                }
+                break;
+            case APDataType::SIGNED_INT:
+                if(sc.res_argtab[i].data.intdata.number_i64 != sc.exp_argtab[i].data.intdata.number_i64){
+                    er->mark_error();
+                    er->log_it("!!! ERROR: value FIELD MISMATCH");
+                    er->log_it("Index    : " + std::to_string(i));
+                    er->log_it("Result   : value = " + sc.res_argtab[i].data.intdata.number_i64);
+                    er->log_it("Expected : value = " + sc.exp_argtab[i].data.intdata.number_i64);
+                }
+                break;
+            case APDataType::TEXT:
+                if(*sc.res_argtab[i].data.text != *sc.exp_argtab[i].data.text){
+                    er->mark_error();
+                    er->log_it("!!! ERROR: value FIELD MISMATCH");
+                    er->log_it("Index    : " + std::to_string(i));
+                    er->log_it("Result   : value = " + *sc.res_argtab[i].data.text);
+                    er->log_it("Expected : value = " + *sc.exp_argtab[i].data.text);
+                }
+                break;
+            case APDataType::FLAG:
+                if(sc.res_argtab[i].data.flag != sc.exp_argtab[i].data.flag){
+                    er->mark_error();
+                    er->log_it("!!! ERROR: value FIELD MISMATCH");
+                    er->log_it("Index    : " + std::to_string(i));
+                    er->log_it("Result   : value = " + sc.res_argtab[i].data.flag);
+                    er->log_it("Expected : value = " + sc.exp_argtab[i].data.flag);
+                }
+                break;
+            default:
+                break;
+            }
         }
     }
     er->log_it(">>> START OF ARGV <<<");
