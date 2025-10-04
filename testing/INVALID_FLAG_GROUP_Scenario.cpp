84d88446dc797521a3e5c0f145db4669b6536dc1
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-10-04 11:01:40 -0400

    updated INVALID_FLAG_GROUP scenario to support appropiate argument data types in builder and validation methods

--- testing/INVALID_FLAG_GROUP_Scenario.cpp
+++ testing/INVALID_FLAG_GROUP_Scenario.cpp
@@ -7,11 +7,12 @@ void build_INVALID_FLAG_GROUP_scenario(Randomizer* rnd, ScenarioData& sc){
     std::vector<std::string> argv;
     std::string arg_id;
     std::string no_dashes_arg_id;
-    std::string value;
+    std::string value_for_argv;
     std::string flag_value;
     std::string group_buffer;
     std::string error_abbr_arg_id;
     std::string error_full_arg_id;
+    union data value{};
     size_t rand_idx;
     size_t n_initialized;
     size_t valid_args_for_group;
@@ -21,9 +22,6 @@ void build_INVALID_FLAG_GROUP_scenario(Randomizer* rnd, ScenarioData& sc){
     int32_t error_abbr_arg_idx;
     bool result_bool;
     bool use_flag_value;
-
-    // Set expected error message 
-    sc.exp_error_message = "";
 
     // Make room in the accumulator argv
     arg_id_accumulator.reserve(sc.n_args);
@@ -139,7 +137,7 @@ void build_INVALID_FLAG_GROUP_scenario(Randomizer* rnd, ScenarioData& sc){
                     continue;
                 }
                 arg_table_idx = arg_table_find_arg_index(sc.exp_argtab, arg_id, true);
-                sc.exp_argtab[arg_table_idx].value = "1";
+                sc.exp_argtab[arg_table_idx].data.flag = true;
             }
             // Update the argv vector with the group argument
             argv.push_back(group_buffer);
@@ -158,43 +156,60 @@ void build_INVALID_FLAG_GROUP_scenario(Randomizer* rnd, ScenarioData& sc){
             }
             // Generate data for arguments that need it
             switch (sc.exp_argtab[arg_table_idx].data_type){
-            case APDataType::NUMBER:
+            case APDataType::UNSIGNED_INT:
+                value.intdata.number_u64 = rnd->gen_integral<uint64_t>();
                 // Pick between hex or decimal
                 result_bool = rnd->gen_bool();
                 if(result_bool){
-                    value = integer_to_hex_string(rnd->gen_integral<int64_t>());
+                    value_for_argv = integer_to_hex_string(value.intdata.number_u64);
                 }
                 else{
-                    value = std::to_string(rnd->gen_integral<int64_t>());
+                    value_for_argv = std::to_string(value.intdata.number_u64);
                 }
                 break;    
             case APDataType::TEXT:
                 result_u32 = rnd->gen_integral_range<uint32_t>(1, MAX_TEXT_ARG_LEN);
-                value = rnd->gen_string(result_u32, nullptr);
-                break;    
+                value.text = new std::string(rnd->gen_string(result_u32, nullptr));
+                value_for_argv = *value.text;
+                break;   
             case APDataType::FLAG:
                 use_flag_value = rnd->gen_bool();
                 // Whether to include a value for FLAG argument or not
                 if(use_flag_value){
                     result_u32 = rnd->gen_integral_range<uint32_t>(0, (sizeof(valid_flag_values) / sizeof(valid_flag_values[0])) -1);
                     flag_value = valid_flag_values[result_u32];
-                    value = valid_flag_values_dict.at(flag_value);
+                    value.flag = valid_flag_values_dict.at(flag_value);
                 }
                 else{
-                    value = "1";
+                    value.flag = true;
                 }
-                break;
+                break;  
             default:
                 break;
             }
 
             // Set argument value
-            sc.exp_argtab[arg_table_idx].value = value;
+            switch(sc.exp_argtab[arg_table_idx].data_type){
+            case APDataType::UNSIGNED_INT:
+                sc.exp_argtab[arg_table_idx].data.intdata.number_u64 = value.intdata.number_u64;
+                break;
+            case APDataType::SIGNED_INT:
+                sc.exp_argtab[arg_table_idx].data.intdata.number_i64 = value.intdata.number_i64;
+                break;
+            case APDataType::TEXT:
+                sc.exp_argtab[arg_table_idx].data.text = new std::string(*value.text);
+                break;
+            case APDataType::FLAG:
+                sc.exp_argtab[arg_table_idx].data.flag = value.flag;
+                break;
+            default:
+                break;
+            }
 
             // Update the argv vector with argument we just created
             argv.push_back(arg_id);
             if(sc.exp_argtab[arg_table_idx].data_type != APDataType::FLAG){
-                argv.push_back(value);
+                argv.push_back(value_for_argv);
             }
             else if(sc.exp_argtab[arg_table_idx].data_type == APDataType::FLAG && use_flag_value){
                 argv.push_back(flag_value);
@@ -202,7 +217,7 @@ void build_INVALID_FLAG_GROUP_scenario(Randomizer* rnd, ScenarioData& sc){
 
             // Update argc appropiately
             switch (sc.exp_argtab[arg_table_idx].data_type){
-            case APDataType::NUMBER:
+            case APDataType::UNSIGNED_INT:
             case APDataType::TEXT:
                 sc.argc += 2;
                 break;
@@ -216,6 +231,11 @@ void build_INVALID_FLAG_GROUP_scenario(Randomizer* rnd, ScenarioData& sc){
                 break;
             default:
                 break;
+            }
+
+            // Deallocate text value if used
+            if(sc.exp_argtab[arg_table_idx].data_type == APDataType::TEXT){
+                delete value.text;
             }
         }
     }
