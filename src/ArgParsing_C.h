6fbcf3a8337dd577feebdb276d543633ad07269e
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-10-11 23:14:36 -0400

    initial implementation of get_instance for C

---
+++ src/ArgParsing_C.h
@@ -0,0 +1,16 @@
+#ifndef ARGPARSING_C
+#define ARGPARSING_C
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef struct ArgParsing_C ArgParsing_C;
+
+ArgParsing_C* get_instance_C();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
