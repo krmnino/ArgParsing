719a490c2e18109dc8032ef39d58f443b07d3203
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-01-18 18:54:21 -0500

    updated BAD_NUMERIC_VALUE scenario build and validation functions

--- testing/BAD_NUMERIC_VALUE_Scenario.cpp
+++ testing/BAD_NUMERIC_VALUE_Scenario.cpp
@@ -55,17 +55,14 @@ std::string generate_bad_number(Randomizer* rnd){
 
 
 void build_BAD_NUMERIC_VALUE_scenario(Randomizer* rnd, ScenarioData& sc){
-    const char* valid_flag_values[] = VALID_FLAG_VALUES;
+    APValuePackage arg_val_package;
     std::vector<std::string> arg_id_accumulator{};
     std::vector<std::string> argv{};
     std::string arg_id{};
     std::string no_dashes_arg_id{};
-    std::string value_for_argv{};
     std::string flag_value{};
-    APValue loc_value{};
     size_t rand_idx{};
     size_t n_initialized{};
-    uint32_t result_u32{};
     int32_t error_table_idx{};
     int32_t arg_table_idx{};
     bool result_bool{};
@@ -170,113 +167,52 @@ void build_BAD_NUMERIC_VALUE_scenario(Randomizer* rnd, ScenarioData& sc){
             no_dashes_arg_id = arg_id.substr(1);
             arg_table_idx = arg_table_find_arg_index(sc.exp_argtab, no_dashes_arg_id, true);
         }
-        // Generate data for arguments that need it
-        use_flag_value = false;
-        switch (sc.exp_argtab[arg_table_idx].data_type){
-        case APDataType::UNSIGNED_INT:
-            // It is time to inject the error
-            if(error_table_idx == arg_table_idx){
-                value_for_argv = generate_bad_number<uint64_t>(rnd);
-            }
-            else{
-                loc_value.number_u64 = rnd->gen_integral<uint64_t>();
-                // Pick between hex or decimal
-                result_bool = rnd->gen_bool();
-                if(result_bool){
-                    value_for_argv = integer_to_hex_string<uint64_t>(loc_value.number_u64);
-                }
-                else{
-                    value_for_argv = std::to_string(loc_value.number_u64);
-                }
-            }
-            break;    
-        case APDataType::SIGNED_INT:
-            // It is time to inject the error
-            if(error_table_idx == arg_table_idx){
-                value_for_argv = generate_bad_number<int64_t>(rnd);
-            }
-            else{
-                loc_value.number_i64 = rnd->gen_integral<int64_t>();
-                // Pick between hex or decimal
-                result_bool = rnd->gen_bool();
-                if(result_bool){
-                    value_for_argv = integer_to_hex_string<int64_t>(loc_value.number_i64);
-                }
-                else{
-                    value_for_argv = std::to_string(loc_value.number_i64);
-                }
-            }
-            break; 
-        case APDataType::TEXT:
-            result_u32 = rnd->gen_integral_range<uint32_t>(1, MAX_TEXT_ARG_LEN);
-            loc_value.text = std::make_shared<std::string>(rnd->gen_string(result_u32, nullptr));
-            value_for_argv = *loc_value.text;
-            break;     
-        case APDataType::FLAG:
-            use_flag_value = rnd->gen_bool();
-            // Whether to include a value for FLAG argument or not
-            if(use_flag_value){
-                result_u32 = rnd->gen_integral_range<uint32_t>(0, (sizeof(valid_flag_values) / sizeof(valid_flag_values[0])) - 1);
-                flag_value = valid_flag_values[result_u32];
-                loc_value.flag = valid_flag_values_dict.at(flag_value);
-            }
-            else{
-                loc_value.flag = true;
-            }
-            break;     
-        default:
-            break;
-        }
 
-        // Set argument value
-        switch(sc.exp_argtab[arg_table_idx].data_type){
-        case APDataType::UNSIGNED_INT:
-            sc.exp_argtab[arg_table_idx].value.number_u64 = loc_value.number_u64;
-            break;
-        case APDataType::SIGNED_INT:
-            sc.exp_argtab[arg_table_idx].value.number_i64 = loc_value.number_i64;
-            break;
-        case APDataType::TEXT:
-            sc.exp_argtab[arg_table_idx].value.text = std::make_shared<std::string>(*loc_value.text);
-            break;
-        case APDataType::FLAG:
-            sc.exp_argtab[arg_table_idx].value.flag = loc_value.flag;
-            break;
-        default:
-            break;
+        // It is time to inject the error
+        if(error_table_idx == arg_table_idx){
+            switch (sc.exp_argtab[arg_table_idx].data_type){
+            case APDataType::UNSIGNED_INT:
+                arg_val_package.stringified = generate_bad_number<uint64_t>(rnd);
+                break;
+            case APDataType::SIGNED_INT:
+                arg_val_package.stringified = generate_bad_number<int64_t>(rnd);
+                break;
+            default:
+                break;
+            }
+        }
+        else{
+            // Generate data for arguments that need it
+            arg_val_package.data_type = sc.exp_argtab[arg_table_idx].data_type;
+            arg_val_package.to_string = true;
+            gen_arg_value(rnd, arg_val_package);
+    
+            // Set argument value
+            copy_APValue(arg_val_package.apv, sc.exp_argtab[arg_table_idx].value, sc.exp_argtab[arg_table_idx].data_type);
+
         }
 
         // Update the argv vector with argument we just created
+        // Update argc appropiately
         argv.push_back(arg_id);
         if(sc.exp_argtab[arg_table_idx].data_type != APDataType::FLAG){
-            argv.push_back(value_for_argv);
-        }
-        else if(sc.exp_argtab[arg_table_idx].data_type == APDataType::FLAG && use_flag_value){
-            argv.push_back(flag_value);
-        }
-
-        // Update argc appropiately
-        switch (sc.exp_argtab[arg_table_idx].data_type){
-        case APDataType::UNSIGNED_INT:
-        case APDataType::SIGNED_INT:
-        case APDataType::TEXT:
+            argv.push_back(arg_val_package.stringified);
             sc.argc += 2;
-            break;
-        case APDataType::FLAG:
-            if(use_flag_value){
+        }
+        else{
+            use_flag_value = rnd->gen_bool();
+            if(use_flag_value || !sc.exp_argtab[arg_table_idx].value.flag){
+                argv.push_back(arg_val_package.stringified);
                 sc.argc += 2;
             }
             else{
                 sc.argc++;
             }
-            break;
-        default:
-            break;
         }
 
         // Set expected error message 
         if(error_table_idx == arg_table_idx){
-            sc.exp_error_message = APErrRsn_to_string(APErrRsn::BAD_NUMERIC_VALUE) + "" ": \"" + value_for_argv +
+            sc.exp_error_message = APErrRsn_to_string(APErrRsn::BAD_NUMERIC_VALUE) + "" ": \"" + arg_val_package.stringified +
                                    "\" provided to the argument --" + sc.exp_argtab[error_table_idx].full_form +
                                    " is not a valid numeric value.";
         }
@@ -290,29 +226,21 @@ void build_BAD_NUMERIC_VALUE_scenario(Randomizer* rnd, ScenarioData& sc){
 void validate_BAD_NUMERIC_VALUE_scenario(ErrorReporter* er, ScenarioData& sc){
     std::string buffer{};
 
-    // Result vs. Expected error mesage
-    er->log_it("Result   : res_error_message = \"" + sc.res_error_message + "\"");
-    er->log_it("Expected : exp_error_message = \"" + sc.exp_error_message + "\"");
-    if(sc.res_error_message != sc.exp_error_message){
-        er->mark_error();
-        er->log_it("!!! ERROR: error_message MISMATCH");
-    }
-    er->log_it(">>> START OF ARGV <<<");
-    er->log_it(describe_argv(sc.argc, sc.argv));
-    er->log_it(">>> END OF ARGV <<<");
-    // Result vs. Expected argument table size
-    er->log_it("Result   : size of result argtab = " + std::to_string(sc.res_argtab.size()));
-    er->log_it("Expected : size of result argtab = " + std::to_string(sc.exp_argtab.size()));
-    if(sc.res_argtab.size() != sc.exp_argtab.size()){
-        er->mark_error();
-        er->log_it("!!! ERROR: size of argtab MISMATCH");
-    }
-    er->log_it(">>> START OF RESULT ARGUMENT TABLE <<<");
-    buffer = arg_table_to_string(sc.res_argtab);
-    er->log_it(buffer);
-    er->log_it(">>> END OF RESULT ARGUMENT TABLE <<<");
     er->log_it(">>> START OF EXPECTED ARGUMENT TABLE <<<");
     buffer = arg_table_to_string(sc.exp_argtab);
     er->log_it(buffer);
     er->log_it(">>> END OF EXPECTED ARGUMENT TABLE <<<");
+    er->log_it(">>> START OF ARGV <<<");
+    er->log_it(describe_argv(sc.argc, sc.argv));
+    er->log_it(">>> END OF ARGV <<<");
+    er->log_it(">>> START OF RESULT ARGUMENT TABLE <<<");
+    buffer = arg_table_to_string(sc.res_argtab);
+    er->log_it(buffer);
+    er->log_it(">>> END OF RESULT ARGUMENT TABLE <<<");
+
+    // Result vs. Expected error mesage
+    validate_error_msg(er, sc.res_error_message, sc.exp_error_message);
+    
+    // Result vs. Expected argument tables (excluding values)
+    validate_arg_table_ex_values(er, sc.res_argtab, sc.exp_argtab);
 }
